745ec1e89029359f8bb9c8bba5ee5ba7ef6868f6
Han Wang <hyw2@cornell.edu>
2022-04-25 00:03:49 -0400

    Update next version

--- playground/pages/_app.tsx
+++ playground/pages/_app.tsx
@@ -1,8 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function Playground({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default Playground
