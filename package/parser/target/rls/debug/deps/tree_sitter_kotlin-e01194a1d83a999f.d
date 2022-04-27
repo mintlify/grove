3923a3ddc0c7a9451eeb184e9608b55152ff4326
Han Wang <hyw2@cornell.edu>
2022-04-27 14:26:46 -0400

    Revert "Remove target folder"
    This reverts commit 1886f836b4d1915398bcc9422ca28923c9b8a6c3.

--- package/parser/target/rls/debug/deps/tree_sitter_kotlin-e01194a1d83a999f.d
+++
@@ -1,6 +0,0 @@
-/Users/hanwang/Desktop/mintlify/grove/package/parser/target/rls/debug/deps/tree_sitter_kotlin-e01194a1d83a999f.rmeta: /Users/hanwang/.cargo/registry/src/github.com-1ecc6299db9ec823/tree-sitter-kotlin-0.2.11/bindings/rust/lib.rs /Users/hanwang/.cargo/registry/src/github.com-1ecc6299db9ec823/tree-sitter-kotlin-0.2.11/bindings/rust/../../src/node-types.json
-
-/Users/hanwang/Desktop/mintlify/grove/package/parser/target/rls/debug/deps/tree_sitter_kotlin-e01194a1d83a999f.d: /Users/hanwang/.cargo/registry/src/github.com-1ecc6299db9ec823/tree-sitter-kotlin-0.2.11/bindings/rust/lib.rs /Users/hanwang/.cargo/registry/src/github.com-1ecc6299db9ec823/tree-sitter-kotlin-0.2.11/bindings/rust/../../src/node-types.json
-
-/Users/hanwang/.cargo/registry/src/github.com-1ecc6299db9ec823/tree-sitter-kotlin-0.2.11/bindings/rust/lib.rs:
-/Users/hanwang/.cargo/registry/src/github.com-1ecc6299db9ec823/tree-sitter-kotlin-0.2.11/bindings/rust/../../src/node-types.json:
